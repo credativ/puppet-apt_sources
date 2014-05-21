ab08b1886b23deba7702355794025ab6679e7de9
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2014-05-21 09:33:51 +0200

    Add debianization

---
+++ debian/puppet-module-aptsources/etc/puppet/module/apt_sources/manifests/params.pp
@@ -0,0 +1,5 @@
+class apt_sources::params {
+    $ensure = 'present'
+    $repos  = {}
+}
+
---
+++ debian/puppet-module-aptsources/etc/puppet/module/apt_sources/manifests/source.pp
@@ -0,0 +1,33 @@
+define apt_sources::source(
+    $ensure,
+    $location,
+    $release,
+    $repos,
+    $required_packages = undef,
+    $key = undef,
+    $key_server = undef,
+    $pin = undef,
+    $include_src = undef,
+    $key_source = undef
+) {
+
+    if $key_source {
+        apt::key { $title:
+            ensure      => $ensure,
+            key_source  => $key_source,
+            before      => Apt::Source[$title]
+        }
+    }
+
+    apt::source { $title:
+	ensure		    => $ensure,
+        location            => $location,
+        release             => $release,
+        repos               => $repos,
+        required_packages   => $required_packages,
+        key                 => $key,
+        key_server          => $key_server,
+        pin                 => $pin,
+        include_src         => $include_src,
+    }
+}
